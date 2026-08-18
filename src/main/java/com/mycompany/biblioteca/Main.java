4e3e92607c374dddd20404f13ad924cb57429cca
Daniel Josué Arrieta <djosuearrieta@unicesar.edu.co>
2026-08-17 19:21:32 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author Daniel
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aquí irá el menú (Fase 8)
     }
+    
+        static void crearCliente() {
+        System.out.print("ID: ");
+        String id = sc.nextLine();
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Telefono: ");
+        String telefono = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        clientes.add(new Cliente(id, nombre, telefono, email));
+        System.out.println("Cliente creado con éxito.");
+    }
+    
 }
